c48c08ee4f99c8b205bc018a37886b25a8ef98a4
Bouhdiallo <bouhdiallo22@gmail.com>
2023-10-25 23:00:57 +0000

    fin projet

--- projetpoo.php
+++ projetpoo.php
@@ -1,8 +1,6 @@
 <?php
-
-//representez les entites "etudiants" et "professeur" suivant le poo en PHP
-
-Class etudiant {
+//representons l' entite etudiant
+class Etudiant {
 
 //proprieté de etudiant
 private $nom;
@@ -25,8 +23,11 @@ public function getNom(){
 }
 
 public function setNom($nouveauNom){
-
-	$this->nom = $nouveauNom;
+    if (is_string($nouveauNom)) {
+        $this->nom = $nouveauNom;
+    }else {
+        throw new exception("le nom doit etre une chaine de caractere");
+    }
 }
 
 
@@ -36,7 +37,11 @@ public function getPrenom(){
 
 public function setPrenom($nouveauPrenom){
 
-	$this->prenom = $nouveauPrenom;
+	if (is_string($nouveauPrenom)) {
+        $this->prenom = $nouveauPrenom;
+    }else {
+        throw new exception("le prenom doit etre une chaine de caractere");
+    }
 }
 
 public function getMatricule(){
@@ -44,21 +49,32 @@ public function getMatricule(){
 }
 
 public function setMatricule($nouveauMatricule){
-
-	$this->matricule = $nouveauMatricule;
+    if (is_string($nouveauMatricule)) {
+        $this->matricule = $nouveauMatricule;
+    }else {
+        throw new exception("le matricule doit avoir des chiffres et des lettres");
+    }
 }
+
+
 
 public function presEtudiant(){
 
 		echo "bonjour<br> mon nom c'est $this->nom <br> mon prenom $this->prenom, <br> ma matricule $this->matricule <br> je suis née le $this->dateNaissance" ."<br><br>";	
 }
+
+
 }
 
-//$etudiant = new Etudiant("fall","ndiaga","PD1205" ,"2003/05/14");
-//$etudiant->presEtudiant();
+      //interface pour professeur
+      interface InterfaceProf {
+        public function presenter();
+        public function evaluerEtudiant($dateEvaluation);
 
+      }
 
-Class professeur extends etudiant{
+//representons l' entite professeur
+class Professeur extends Etudiant implements InterfaceProf{
 
     public $voiture;
     public $salaire;
@@ -72,18 +88,29 @@ Class professeur extends etudiant{
         $this->salaire = $monSalaire;
         $this->specialite = $maSpecialite;
     }
+
     public function presProfesseur() {
-		echo "bonjour<br> mon nom c'est ".$this->getNom()." <br> mon prenom ".$this->getPrenom()." <br> j'ai une $this->voiture <br> et une salaire de $this->salaire par mois <br> je suis $this->specialite <br> ma matricule ".$this->getMatricule()." <br> je suis née le $this->dateNaissance.";		
+		echo "bonjour<br> mon nom c'est ".$this->getNom()." <br> mon prenom ".$this->getPrenom()." <br> j'ai une $this->voiture <br> et une salaire de $this->salaire par mois <br> je suis $this->specialite <br> ma matricule ".$this->getMatricule()." <br> je suis née le $this->dateNaissance. <br>";		
+    }
+
+    
+    public function presenter(){
+        echo "Salut, je suis professeur, je m’appelle ".$this->getNom()." ".$this->getPrenom().", spécialisé dans le domaine de $this->specialite, j’ai une voiture $this->voiture, j’ai comme salaire $this->salaire FCFA <br>";
+    }
+    public function evaluerEtudiant($dateEvaluation) {
+        echo "chers etudiants je vous evalue le $dateEvaluation";
     }
 }
 
 
-$etudiant = new Etudiant("fall","ndiaga","PD1205" ,"2003/05/14");
+$etudiant = new Etudiant("ndiaye","ndiaga","PD1205" ,"2003/05/14");
 $etudiant->presEtudiant();
-
 
 $professeur = new Professeur ("kane","samba","AD5050","2000/05/04","rangerRover",800000,"designer",);
 $professeur->presProfesseur();
+
+$professeur->presenter();
+$professeur->evaluerEtudiant("2023/05/28");
 
 
 
